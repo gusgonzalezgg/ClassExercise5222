4d8cce0cd44f2406cb607d753db936ede4b4c00f
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-05-04 15:41:30 -0400

    submitting exercise

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassExercise5222
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What was your first car?");
+            Console.WriteLine();
+
+
+            Car myFirstCar = new Car()
+            {
+                Year = 2005,
+                Make = "Ford",
+                Model = "Escape",
+            };
+
+            Console.WriteLine(myFirstCar.Year);
+            Console.WriteLine(myFirstCar.Make);
+            Console.WriteLine(myFirstCar.Model);
+
+        }
+    }
+}
